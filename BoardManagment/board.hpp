348ab8533773ba1bb6a30ecd8487461f2f4b3114
Lukawie98 <124913483+Lukawie98@users.noreply.github.com>
2023-10-17 14:30:54 +0200

    Update board.hpp

--- BoardManagment/board.hpp
+++ BoardManagment/board.hpp
@@ -12,4 +12,5 @@ class Board //Basic class for creating different sizes of boards
     
 public:
     virtual void showBoard(std::vector<Player> & p) = 0;  //Virtual function, displays the player board depending on the number of players
+    virtual ~Board(){}
 };
